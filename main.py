4c0ba706fffb8a16602b21de690d0c198124d9ab
tonnyhatter <steven_anthony_hall@yahoo.com>
2025-07-16 00:14:24 -0700

    D: minor refactoring

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-    while SCREEN_WIDTH == 1280:
+    running = True
+    while running:
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+
         screen.fill((0,0,0))
+        player.update(dt)
         player.draw(screen)
-        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
